133d0fb1d1c7a8c76cd61ba39dbe089b83d2bca8
unknown <kaj61@254-1.cslabs.cs.uakron.edu>
2016-02-15 13:16:13 -0500

    Implemented File->Read Option, now need to read meta data from DLL

--- calc/Form1.cs
+++ calc/Form1.cs
@@ -19,7 +19,46 @@ namespace calc
 
         private void Form1_Load(object sender, EventArgs e)
         {
+        // make combo box non editable (top left).
+        comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+   
+        }
 
+        /*prompts user to load in DLL file, enforces DLL */
+        private void displayFileStuff()
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "DLL | *.dll";
+            openFile.ShowDialog();
+        }
+
+        private void openFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            /*Once calc.dll is opened -- use Reflection API to display
+             * all class names from DLL as well as parameters and methods 
+             */
+            /* 
+             * Allow user to pick any number of methods to construct a calulator, dynamically
+             */
+            /*
+            this.Activate();
+            file = openFileDialog.OpenFile();
+            System.IO.FileInfo fileInfo = new System.IO.FileInfo(file);
+            System.IO.FileStream fileStream = fileInfo.OpenRead();
+            fileStream.Close(); */
+        }
+
+        private void openFileDialog_FileOk_1(object sender, CancelEventArgs e)
+        {
+
+
+        }
+
+        //when you change Top right from File to Open
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            /*When forms loads, prompt user to find the DLL file */
+             displayFileStuff();
         }
     }
 }
